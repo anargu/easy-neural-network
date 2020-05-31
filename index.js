122c9798f7a3c54b036bf1a86dd6f028d87b71f4
anargu <anthony.arostegui@gmail.com>
2020-05-31 12:49:23 -0500

    backpropagation coded

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-let nn = new NeuralNetwork(3,2,2);
+let nn = new NeuralNetwork(2,2,1);
 
 console.log('hidden_layer', nn.hidden_layer);
 console.log('output_layer', nn.output_layer);
@@ -7,5 +7,46 @@ console.log('output_layer', nn.output_layer);
 // let output = nn.feedforward([0.1, 0.5, 0.6]);
 // console.log('output',output[0]);
 
-let result = nn.train([0.1, 0.2, 0.3], [0.5, 1]);
+// let result = nn.trainV2([0.1, 0.2, 0.3], [0.5, 1]);
 // console.log('result', result);
+
+let training_data = [
+  {
+    inputs: [0, 0],
+    targets: [0]
+  },
+  {
+    inputs: [1, 0],
+    targets: [1]
+  },
+  {
+    inputs: [0, 1],
+    targets: [1]
+  },
+  {
+    inputs: [1, 1],
+    targets: [0]
+  }
+]
+
+// nn.trainV2([1,1],[0]);
+// nn.trainV2([0,0],[0]);
+// nn.trainV2([0,1],[1]);
+// nn.trainV2([1,0],[1]);
+
+for(let i = 0; i < 10000; i++) {
+  // let data = random(training_data);
+  // nn.trainV2(data.inputs, data.targets);
+  for (data of training_data) {
+    nn.trainV2(data.inputs, data.targets);
+  }
+}
+
+let result = nn.feedforward([0,0]);
+let result1 = nn.feedforward([1,0]);
+let result2 = nn.feedforward([0,1]);
+let result3 = nn.feedforward([1,1]);
+console.log('result [0,0]',result[0]._data[0][0]);
+console.log('result1 [1,0]',result1[0]._data[0][0]);
+console.log('result2 [0,1]',result2[0]._data[0][0]);
+console.log('result3 [1,1]',result3[0]._data[0][0]);
--- libs/deep.js
+++ libs/deep.js
@@ -1,12 +1,24 @@
 const LR = 0.1;
 
+function checkNaN(mattr) {
+    // math.isNaN(this.hidden_layer)
+    mattr.forEach((item) => {
+      if(item === true){
+        return true
+      }
+    });
+    return false;
+}
+
 class NeuralNetwork {
   constructor(i_nodes, h_nodes, o_nodes) {
-    this.input_nodes = i_nodes + 1;
-    this.hidden_nodes = h_nodes + 1;
+    this.input_nodes = i_nodes;
+    this.hidden_nodes = h_nodes;
     this.output_nodes = o_nodes;
 
 
+    this.ih_bias = this.createWMatrix(1, this.hidden_nodes);
+    this.ho_bias = this.createWMatrix(1, this.output_nodes);
     this.hidden_layer = this.createWMatrix(this.input_nodes, this.hidden_nodes);
     this.output_layer = this.createWMatrix(this.hidden_nodes, this.output_nodes);
   }
@@ -26,17 +38,71 @@ class NeuralNetwork {
   // HxI - Ix1 => Hx1
   // OxH - Hx1 => Ox1
   // inputs => [..input..]
-  feedforward(inputs) {
-    inputs = inputs.concat([1]);
+  feedforward(inputs, extended=false) {
     let I = math.matrix([inputs]);
     I = math.transpose(I);
     const H = this.hidden_layer;
+    const BI = this.ih_bias;
 
-    const H1 = NeuralNetwork.activation(math.multiply(H, I));
+    let hib = math.multiply(H, I);
+
+    hib = math.add(hib, BI);
+
+    const H1 = NeuralNetwork.activation(hib);
 
     const O = this.output_layer;
-    const O1 = NeuralNetwork.activation(math.multiply(O, H1));
-    return [O1, H1];
+    let Z = math.multiply(O, H1);
+    const BH = this.ho_bias;
+    Z = math.add(Z, BH);
+    const O1 = NeuralNetwork.activation(Z);
+    if(extended === true) {
+      return [O1, Z, O, H1, hib, H];
+    } else {
+      return [O1, H1];
+    }
+  }
+
+  trainV2(inputs, targets) {
+    let [O1, Z, O, H1, Zi, H] = this.feedforward(inputs, true);
+    const I = math.matrix([inputs]);
+    const target = math.transpose(math.matrix([targets]));
+    let o_errors = math.subtract(target, O1);
+    let dz = Z.map(NeuralNetwork.dsigmoid);
+
+    const delta = math.dotMultiply(dz, o_errors);
+    // delta 2x1
+    // H1 2x1
+    let grad_desc = math.multiply(delta, math.transpose(H1))
+    let grad_desb_b = delta;
+
+    grad_desc = math.multiply(grad_desc, LR);
+    grad_desb_b = math.multiply(grad_desb_b, LR);
+
+    // console.log('grad_desc', grad_desc);
+
+    this.output_layer = math.add(this.output_layer, grad_desc);
+    this.ho_bias = math.add(this.ho_bias, grad_desb_b);
+
+    let deltaW = math.multiply(delta, this.output_layer);
+    let dzi = Zi.map(NeuralNetwork.dsigmoid);
+    const deltaI = math.dotMultiply(math.transpose(dzi), deltaW);
+
+    let grad_desc_h = math.multiply(math.transpose(deltaI), I);
+    let grad_desb_hb = math.transpose(deltaI);
+    grad_desc_h = math.multiply(grad_desc_h, LR);
+    grad_desb_hb = math.multiply(grad_desb_hb, LR);
+
+    this.hidden_layer = math.add(this.hidden_layer, grad_desc_h);
+    if(checkNaN(math.isNaN(this.hidden_layer))) {
+      throw new Error(`item is NaN ${item}`);
+    }
+
+    this.ih_bias = math.add(this.ih_bias, grad_desb_hb);
+    if(checkNaN(math.isNaN(this.hidden_layer))) {
+      throw new Error(`item is NaN ${item}`);
+    }
+
+    // console.log('output_layer', this.output_layer);
   }
 
   train(inputs, targets) {
